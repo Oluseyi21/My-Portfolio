87384c92ed80c1041c1775528c23a8570fe9bfa0
Oluseyi21 <oluseyisosanya@gmail.com>
2026-03-13 22:12:56 -0400

    changed the js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('.nav-links');
+document.addEventListener("DOMContentLoaded", () => {
+    const homeText = document.querySelector(".home-text");
+    const homeImg = document.querySelector(".home-img img");
 
-menuIcon.onClick = () => {
-    navLinks.classList.toggle('active');
-}
+    // Fade in text
+    homeText.style.opacity = 0;
+    homeText.style.transition = "opacity 1s ease-in-out";
+    setTimeout(() => { homeText.style.opacity = 1; }, 300);
+
+    // Slide in image
+    homeImg.style.transform = "translateX(100px)";
+    homeImg.style.opacity = 0;
+    homeImg.style.transition = "all 1s ease-in-out";
+    setTimeout(() => { 
+        homeImg.style.transform = "translateX(0)";
+        homeImg.style.opacity = 1;
+    }, 500);
+});
